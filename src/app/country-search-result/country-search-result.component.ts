628ee6e132e74516ac72f74add717bfbb1e37bed
guy <guyasulin13@gmail.com>
2020-11-17 12:38:05 +0200

    change css

--- src/app/country-search-result/country-search-result.component.ts
+++ src/app/country-search-result/country-search-result.component.ts
@@ -1,18 +1,36 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Country } from '../models/country';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
-  selector: 'app-country-search-result',
-  templateUrl: './country-search-result.component.html',
-  styleUrls: ['./country-search-result.component.scss']
+	selector: 'app-country-search-result',
+	templateUrl: './country-search-result.component.html',
+	styleUrls: [ './country-search-result.component.scss' ],
+	animations: [
+		trigger('openItemAnimation', [
+			transition('void => *', [
+				animate(
+					'1s',
+					keyframes([
+						style({
+							opacity: 0,
+							transform: 'translateY(500px)'
+						}),
+						style({
+							opacity: 1,
+							transform: 'translateY(0)'
+						})
+					])
+				)
+			])
+		])
+	]
 })
 export class CountrySearchResultComponent implements OnInit {
+	@Input() country: Country;
+	public state: string;
 
-  @Input() country: Country;
-  
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
